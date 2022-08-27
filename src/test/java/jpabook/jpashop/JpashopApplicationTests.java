e77282f327227a2b75848869658b6d698eb2c057
Cha <cktmddus11@naver.com>
2022-08-27 20:08:44 +0900

    머지테스트5

--- src/test/java/jpabook/jpashop/JpashopApplicationTests.java
+++ src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class JpashopApplicationTests {
 
-	@Test
+	@Test ///으아아아아아아
 	void contextLoads() {
 	}
 // 머지테스트 //하나더 커밋 //커밋...도대체 어케 하는거여
